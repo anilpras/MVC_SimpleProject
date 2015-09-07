23ae9407ee6f66aa9a07c65c8ec4a97feca13cd6
ryot <anilpras@microsoft.com>
2015-09-07 14:37:56 +0530

    changes in log _2

--- MVC_WithoutDataBase/Controllers/HomeController.cs
+++ MVC_WithoutDataBase/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MVC_WithoutDataBase.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application, published to github";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application, published to github_test";
 
             return View();
         }
